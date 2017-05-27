fc69ce7a05697b8b4dbc780b573fcb47e5201aad
SJIRJIS <sjirjis@gmail.com>
2017-05-27 02:07:03 -0700

    uncommented scrapeController for production

--- controller/scrapeController.js
+++ controller/scrapeController.js
@@ -3,44 +3,39 @@ var cheerio = require('cheerio'),
     Scrape = require('../model/Scrape'),
     Promise = require('promise');
 
-// Scrape.remove(function(err){
-// 	if (err) throw err;
-// 	else console.log('collection removed');
-// });
-
 module.exports = function(app) {
 
-	// function scraper() {		
-	// 	return new Promise(function(fulfill, reject) {       
-	// 	    request("https://www.washingtonpost.com/", function(error, response, html) {
-	// 	        // Load the html body from request into cheerio
-	// 	        var $ = cheerio.load(html);
+	function scraper() {		
+		return new Promise(function(fulfill, reject) {       
+		    request("https://www.washingtonpost.com/", function(error, response, html) {
+		        // Load the html body from request into cheerio
+		        var $ = cheerio.load(html);
 
-	// 	        // For each element with a "headline" class
-	//             $(".headline").each(function(i, element) {
+		        // For each element with a "headline" class
+	            $(".headline").each(function(i, element) {
 	                
-	//                 // Save the text of each url enclosed in the current element
-	//                 var headline = $(this).children("a").text();
-	//                 // Save the href value of each url enclosed in the current element
-	//                 var url = $(this).children("a").attr("href");
+	                // Save the text of each url enclosed in the current element
+	                var headline = $(this).children("a").text();
+	                // Save the href value of each url enclosed in the current element
+	                var url = $(this).children("a").attr("href");
 
-	//                 // If this headline element had both a headline and a url
-	//                 if (headline && url) {
-	//                     var newItem = new Scrape({
-	//                         headline: headline,
-	//                         url: url
-	//                     });
-	//                     // Save the data in the scrapedData db
-	//                     newItem.save();
-	//                 }
-	//             });
-	//         })
-	// 	fulfill();
-	// 	});
-	// }; 
+	                // If this headline element had both a headline and a url
+	                if (headline && url) {
+	                    var newItem = new Scrape({
+	                        headline: headline,
+	                        url: url
+	                    });
+	                    // Save the data in the scrapedData db
+	                    newItem.save();
+	                }
+	            });
+	        })
+		fulfill();
+		});
+	}; 
 
 	app.get('/scrape', function(req, res) {
-		//scraper().then(function(data) {
+		scraper().then(function(data) {
 		    // This will get the articles we scraped from the mongoDB
 		    // Grab every doc in the Articles array
 		    Scrape.find(function(error, doc) {
@@ -54,6 +49,6 @@ module.exports = function(app) {
 		            res.send(doc);
 		        }
 		    });
-		//});
+		});
 	})		
 };
